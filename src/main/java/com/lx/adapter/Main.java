41f003b53d6fa853d5997f559114b06c1eb88b97
liuxiao <956788625@qq.com>
2021-01-08 20:23:58 +0800

    适配器模式

---
+++ src/main/java/com/lx/adapter/Main.java
@@ -0,0 +1,18 @@
+package com.lx.adapter;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        FileInputStream fileInputStream = new FileInputStream("D:\\test.txt");
+        InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        String line;
+        while ((line = bufferedReader.readLine()) != null ) {
+            System.out.println(line);
+        }
+        bufferedReader.close();
+    }
+}
